4178876de0f6c61f51607ead7dae03628150b4c5
cyren4 <Cyren4@users.noreply.github.com>
2020-12-25 18:25:41 +0100

    try jframe

--- ProjetS1/quidditch/src/quidditch/Joueur.java
+++ ProjetS1/quidditch/src/quidditch/Joueur.java
@@ -1,5 +1,16 @@
 package quidditch;
-
+	
 public class Joueur {
-
+	private float x;
+	private float y;
+	public final String nom;
+	public final int team;
+	
+	public Joueur(String nom, int team) {
+		this.nom = nom;
+		this.team = team;
+		x = team == 1 ? 175 : 250;
+		y = 100;
+		}
+	
 }
